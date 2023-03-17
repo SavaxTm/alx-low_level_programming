fc4d3cf0b2c098be09076229cdf09dbc6679b4f4
SavaxTm <savax172@gmail.com>
2023-03-16 20:07:51 -0700

    df

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <conio.h>
+main()
+{
+	clrscr();
+	printf("Size of a char: %2d byte(s) \n", sizeof(char));
+	printf("Size of an int: %2d byte(s) \n", sizeof(int));
+	printf("Size of a long int: %2d byte(s) \n", sizeof(longint));
+	printf("Size of a long long int: %2d byte(s) \n", sizeof(long long int));
+	printf("Size of a float: %2d byte(s) \n", sizeof(float));
+	getch()
