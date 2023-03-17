bd6ac4215e9df456d540f3a7e3c3732e0b6e59bf
SavaxTm <savax172@gmail.com>
2023-03-17 12:16:44 -0700

    sa

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,15 +4,9 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
-int main(void)/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+int main(void)/* Main function */
 {
-	int n;/**
-		* main - Entry point
-		*/
+	int n;
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
