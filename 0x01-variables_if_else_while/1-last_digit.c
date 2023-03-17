b4b0d35dec70fe8bb31a494baa86bce1eb3b7b1f
SavaxTm <savax172@gmail.com>
2023-03-17 13:21:03 -0700

    dsa

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,6 +4,11 @@
 /* more headers goes there */
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
@@ -17,6 +22,22 @@ int main(void)
 		printf("%s", " is ");
 		printf("%d", n%10);
 		printf("%s\n", " and is greater than 5");
+	}
+	if(n%10 == 0)
+	{
+		printf("%s", "Last digit of ");
+		printf("%d", n);
+		printf("%s", " is ");
+		printf("%d", n%10);
+		printf("%s\n", " and is 0");
+	}
+	if(n%10 < 6 && n%10 != 0)
+	{
+		printf("%s", "Last digit of ");
+		printf("%d", n);
+		printf("%s", " is ");
+		printf("%d", n%10);
+		printf("%s\n", " and is less than 6 and not 0");
 	}
 	/* your code goes there */
 	return (0);
