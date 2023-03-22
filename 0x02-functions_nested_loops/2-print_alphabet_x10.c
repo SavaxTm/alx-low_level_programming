bceeeec86ef6ed830d6e88faf964a567a46191b0
SavaxTm <savax172@gmail.com>
2023-03-22 21:35:03 +0100

    a

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints the alphabets ten times
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int k;
+	char d;
+
+	for(k = 1; k <= 10; ++k)
+	{
+		for(d = 'a'; d <= 'z'; ++d)
+			_putchar(d);
+		_putchar('\n');
+	}
+}
