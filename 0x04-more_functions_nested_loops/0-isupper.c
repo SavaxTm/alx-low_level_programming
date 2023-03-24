79aad5706912954b48b89686cfa86eb03cd5ca4a
SavaxTm <savax172@gmail.com>
2023-03-24 04:11:18 +0100

    a

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * Return: 1 (Success)
+ * Onerror return 0
+ */
+int main(void)
+{
+	char _isupper;
+
+	if (_isupper = 65, _isupper <= 90)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x04-more_functions_nested_loops/0-main.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * min - check the code.
+ *
+ * Return: Always 0.
+ */
+int min(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c\n", c);
+    c = 'a';
+    printf("%c\n", c);
+    return (0);
+}
