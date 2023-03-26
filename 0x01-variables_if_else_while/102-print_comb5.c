2238dfd54994a032588ed956329fc6337fa4d327
SavaxTm <savax172@gmail.com>
2023-03-26 14:41:13 +0100

    combination of two digits without restrictions

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -10,18 +10,29 @@ int main(void)
 {
 	int x;
 	int y;
+	int i;
+	int j;
 
 	for (x = 48; x <= 57; x++)
 	{
 		for (y = 48; y <= 57; y++)
 		{
-				putchar(x);
-				putchar(y);
-				if (x + y != 114)
+			for (i = 48; i <= 57; i++)
+			{
+				for (j = 48; j <= 57; j++)
 				{
-					putchar(',');
+					putchar(x);
+					putchar(y);
 					putchar(' ');
+					putchar(i);
+					putchar(j);
+					if (x + y + i + j != 228)
+					{
+						putchar(',');
+						putchar(' ');
+					}
 				}
+			}
 		}
 	}
 	putchar('\n');
