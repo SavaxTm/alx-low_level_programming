e88fec4393872c9ba19852af25f9ace35d64a159
SavaxTm <savax172@gmail.com>
2023-03-26 20:16:15 +0100

    print 1-14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * more_numbers - prints the numbers 0-9
+ * more_numbers - prints the numbers 0-14
  *
  * @param i - Parameter that prints the numbers 10 times
- * @param j - Parameter that prints numbers 0-9
+ * @param j - Parameter that prints numbers 0-14
  *
  * Return - void
  */
@@ -14,9 +14,13 @@ void more_numbers(void)
 
 	for (i = 1; i <= 10; i++)
 	{
-		for (j = '0'; j <= '9'; j++)
+		for (j = 0; j < 15; j++)
 		{
-			_putchar(j);
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar(j % 10 + '0');
 		}	
 	_putchar('\n');
 	}
