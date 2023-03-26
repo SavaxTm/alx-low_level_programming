bd35aeaeceb6ec170f1b71871a2c00ffbfdfb235
SavaxTm <savax172@gmail.com>
2023-03-26 19:38:45 +0100

    print 0-9 omitting 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -2,14 +2,13 @@
 /**
  * print_most_numbers - prints numbers
  *
- * @i : Parameter to print 0-9
  *
  * Return: Void
  */
 void print_most_numbers(void)
 {
 	int i;
-	
+
 	for (i = 48; i <= 57; i++)
 		if (i != 50 && i != 52)
 			_putchar(i);
