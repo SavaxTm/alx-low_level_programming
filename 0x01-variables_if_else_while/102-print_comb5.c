438c732aa21a3735de14033ee87acd3e0906d4dc
SavaxTm <savax172@gmail.com>
2023-03-26 14:19:16 +0100

    combination of two digits without restrictions

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description - Printing the poss. comb. of two digits
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x;
+	int y;
+
+	for (x = 48; x <= 57; x++)
+	{
+		for (y = 49; y <= 57; y++)
+		{
+				putchar(x);
+				putchar(y);
+				if (x + y != 114)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
