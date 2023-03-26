d62fc4b0e2618cb46c73cb6783bbc2177576ff9c
SavaxTm <savax172@gmail.com>
2023-03-26 05:45:06 +0100

    print all the digits of base 10

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description - Prints all digits of base 10
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int x = 48;
+
+	while (x <= 57)
+	{
+		putchar (x);
+		x++;
+	}
+	putchar ('\n');
+	return (0);
+}
