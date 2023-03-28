8d7a7778f5ace94436f8a4ec626d67d38e72cf91
SavaxTm <savax172@gmail.com>
2023-03-28 04:53:21 +0100

    Pointers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * swap_int - function to swap values
+ *
+ * Return: void
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+	return;
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -2,4 +2,5 @@
 #define _MAIN_H_
 int main(void);
 void reset_to_98(int *n);
+void swap_int(int *a, int *b);
 #endif
