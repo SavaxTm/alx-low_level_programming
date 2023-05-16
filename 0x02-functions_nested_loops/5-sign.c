4cc2d5e04bb33aa7743ea842323d04ae565268a3
SavaxTm <savax172@gmail.com>
2023-05-16 15:56:58 +0100

    TIB

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
  * print_sign - prints sign of given character
  * @n : parameter for the character to be checked
- * return : 1 for positive numbers, 0 for 0 and -1 for negative numbers
+ * Return : 1 for positive numbers, 0 for 0 and -1 for negative numbers
  */
 int print_sign(int n)
 {
