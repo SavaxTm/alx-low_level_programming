52b2bd87b2133292f4defe8e2a9817321c85783e
SavaxTm <savax172@gmail.com>
2023-05-16 15:54:07 +0100

    TIB

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * print_sign - prints sign of given character
+ * @n : parameter for the character to be checked
+ * return : 1 for positive numbers, 0 for 0 and -1 for negative numbers
+ */
 int print_sign(int n)
 {
 	if (n > 0)
