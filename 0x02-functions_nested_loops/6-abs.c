83ca851586e7d1306bb3c4d0246011fc9a3dd712
SavaxTm <savax172@gmail.com>
2023-05-16 16:27:13 +0100

    TIB

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,4 +1,9 @@
 #include "main.h"
+/**
+ * _abs - Entry Point
+ * @c : parameter that returns absolute value
+ * Return: c if positive and c * -1 if negative
+ */
 int _abs(int);
 int _abs(int c)
 {
