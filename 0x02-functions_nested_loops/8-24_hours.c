020efa2ce4c1ae234bc56a014fc5b90e9ff0b229
SavaxTm <savax172@gmail.com>
2023-05-19 16:37:07 +0100

    Juicy

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -2,14 +2,14 @@
 #include <stdio.h>
 void jack_bauer(void)
 {
-	int a,b;
+	int a, b;
 
 	for (a = 0; a <= 24; a++)
 	{
-		for (b = 0; b <= 60; b++)
+		for (b = 0; b <= 59; b++)
 		{
-			printf ("%02d", a);
-			printf (":%02d\n", b);
+			printf("%02d", a);
+			printf(":%02d\n", b);
 		}
 	}
 }
