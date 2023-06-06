8a9d7f266d445876065de07c818f7da26eeea33a
SavaxTm <savax172@gmail.com>
2023-06-06 11:35:58 +0100

    Finally got it

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+void times_table(void)
+{
+	int a,b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			if (b == 9)
+			{
+				printf("%2d", b * a);
+			}
+			else
+			{
+				printf("%2d, ", b * a);
+			}
+		}
+		printf("\n");
+	}
+	return;
+}
