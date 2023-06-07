f8fd7054a286be5f345b4abde1385c277b482656
SavaxTm <savax172@gmail.com>
2023-06-07 18:25:17 +0100

    times-table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_times_table - prints times table
+ * @n : parameter to input number
+ */
+void print_times_table(int n)
+{
+	int a, b;
+
+	for (a = 0; a <= n; a++)
+	{
+		for (b = 0; b <= n; b++)
+		{
+			if (b == n)
+			{
+				printf("%d", a * b);
+			}
+			else
+			{
+				printf("%4d, ", a * b);
+			}
+		}
+		printf("\n");
+	}
+}	
