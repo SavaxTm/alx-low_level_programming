0d89a82c05bbd41d64be456773ac0a20f07a54cd
SavaxTm <savax172@gmail.com>
2023-07-03 15:39:02 +0100

    done

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _print_rev_recursion - prints the entire characters of a string in reverse order
+ * @s: The current character in the string to be printed
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+		return;
+	}
+}
