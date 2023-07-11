bb9da54f7f5c31a200fa336f469a2f5301a25226
SavaxTm <savax172@gmail.com>
2023-07-11 03:36:43 +0100

    poco a poco

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -19,38 +19,32 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	if (filename == NULL)
 	{
-		return 0;
+		return (0);
 	}
-
 	name = fopen(filename, "r");
 	if (name == NULL)
 	{
-		return 0;
+		return (0);
 	}
-
 	buffer = (char *)malloc((letters + 1) * sizeof(char));
 	if (buffer == NULL)
 	{
 		fclose(name);
-		return 0;
+		return (0);
 	}
-
 	bytesRead = fread(buffer, sizeof(char), letters, name);
 	if (bytesRead <= 0)
 	{
 		free(buffer);
 		fclose(name);
-		return 0;
+		return (0);
 	}
-
 	bytesWritten = write(STDOUT_FILENO, buffer, bytesRead);
 	free(buffer);
 	fclose(name);
-
 	if (bytesWritten != bytesRead)
 	{
-		return 0;
+		return (0);
 	}
-
-	return bytesWritten;
+	return (bytesWritten);
 }
